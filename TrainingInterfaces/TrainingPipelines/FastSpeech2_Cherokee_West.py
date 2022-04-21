4b58a54909f4615479d4bdb19bb6c529d156f656
ᎹᎦᎵ <m.conrad.202@gmail.com>
2022-04-21 09:29:27 -0400

    try 80,000

--- TrainingInterfaces/TrainingPipelines/FastSpeech2_Cherokee_West.py
+++ TrainingInterfaces/TrainingPipelines/FastSpeech2_Cherokee_West.py
@@ -66,7 +66,7 @@ def run(gpu_id, resume_checkpoint, finetune, model_dir, resume, remove_faulty_sa
                datasets=datasets,  #
                batch_size=6,  #
                save_directory=meta_save_dir,  #
-               steps=50_000,  #
+               steps=80_000,  #
                steps_per_checkpoint=1000,  #
                lr=0.001,  #
                path_to_checkpoint=resume_checkpoint,  #
